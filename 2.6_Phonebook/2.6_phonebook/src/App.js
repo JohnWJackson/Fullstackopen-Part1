f3e1fd90951849219f0004279aa6becd45b2982a
Johnwjackson@yahoo.comgit config --global user.name JohnJgit config --global user.email Johnwjackson@yahoo.com <Johnwjackson@yahoo.com>
2022-01-24 12:04:03 -0500

    Working phonebook, displays names

--- 2.6_Phonebook/2.6_phonebook/src/App.js
+++ 2.6_Phonebook/2.6_phonebook/src/App.js
@@ -1,24 +1,51 @@
 import React, { useState } from 'react'
 
+const Person = ({person}) => {
+  return (
+    <li>{person.name}</li>
+  );
+};
+
 const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas' }
   ]) 
   const [newName, setNewName] = useState('')
 
+  const handlePersonChange = (event) => {
+    setNewName(event.target.value);
+  }
+
+  const addPerson = (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+    };
+  
+    setPersons(persons.concat(personObject));
+    setNewName('');
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addPerson}>
         <div>
-          name: <input />
+          name: <input 
+                  value={newName}
+                  onChange={handlePersonChange}
+                />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(p => 
+          <Person key={p.name} person={p} />
+        )}
+      </ul>
     </div>
   )
 }
