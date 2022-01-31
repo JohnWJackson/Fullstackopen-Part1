dd5b4fd745a518bafb9fe6ac66db661d1416d91d
Johnwjackson@yahoo.comgit config --global user.name JohnJgit config --global user.email Johnwjackson@yahoo.com <Johnwjackson@yahoo.com>
2022-01-31 09:54:47 -0500

    API delete persons

--- 3.1_phonebook_backend/index.js
+++ 3.1_phonebook_backend/index.js
@@ -28,13 +28,13 @@ app.get('/', (request, response) => {
   response.send('<h1>Phonebook!</h1>');
 })
 
-app.get('/api/persons', (request, response) => {
-  response.json(notes);
-})
-
 app.get('/info', (request, response) => {
   response.send(`Phonebook has info for ${notes.length} people`
   + '<br/>' + `${Date()}`);
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons);
 })
 
 app.get('/api/persons/:id', (request, response) => {
@@ -46,6 +46,15 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(404).end();
   }
 })
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id);
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(202).end();
+})
+
+
 
 const PORT = 3001
 app.listen(PORT, () => {
